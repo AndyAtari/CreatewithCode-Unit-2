c5b6d0355986d0bd03fb60c829267f39046c90a6
Andy Atari <andrewsalteri@gmail.com>
2021-05-30 15:12:16 -0400

    Added a MoveForward script that moves animals and sandwiches forward at varying speeds. Added if statement in PlayerController that shoots sandwiches when player presses spacebar.

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 20.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,11 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
