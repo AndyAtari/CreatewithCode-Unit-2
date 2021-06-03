2605d491b65cbd8a78f3c8f4f85626e8fc73b5af
Andy Atari <andrewsalteri@gmail.com>
2021-06-02 20:34:11 -0400

    Called InvokeRepeating that calls a method on set time interval. Created a method to encapsulate code that spawns random animal.

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -6,23 +6,28 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
-    private float spawnRangeZ = 40; 
+    private float spawnRangeZ = 100;
+    private float startDelay = 2.0f;
+    private float interval = 1.0f; 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, interval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    // Instantiates random animals 
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
