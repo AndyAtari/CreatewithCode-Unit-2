1107b60b3a79b2344e31bb6cdf090d157aea6bba
Andy Atari <andrewsalteri@gmail.com>
2021-06-04 21:20:58 -0400

    Added if statement that moves the player forward and backward on z axis within boundaries.

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 15.0f;
     public float xRange = 20.0f;
     public GameObject projectilePrefab;
+    public float verticalInput;
+    private float topPlayer = 5.0f;
+    private float lowerPlayer = -10.0f;
 
     void Start()
     {
@@ -26,12 +29,25 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        if (transform.position.z < lowerPlayer)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, lowerPlayer);
+        }
+        else if (transform.position.z > topPlayer)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, topPlayer);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Fire");
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
